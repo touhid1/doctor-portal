f8162d772e848a4e06272df40966e77b7b6b3ca6
touhid1 <touhiduliislam5@gmail.com>
2020-10-13 01:58:26 +0600

    Allhamdulliah

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWA_drFCI9uezpXPrIAMdsc5CkZrjN2vE",
+  authDomain: "doctors-portal-1c0ff.firebaseapp.com",
+  databaseURL: "https://doctors-portal-1c0ff.firebaseio.com",
+  projectId: "doctors-portal-1c0ff",
+  storageBucket: "doctors-portal-1c0ff.appspot.com",
+  messagingSenderId: "492636113798",
+  appId: "1:492636113798:web:666893f42feac2d0a07a07"
+};
+  export default firebaseConfig;
